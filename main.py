b45712573b4d069d02a6162cd5401c3f3f3cab4e
Felipe Ucelli <78266261+felipeucelli@users.noreply.github.com>
2024-08-06 20:01:44 -0400

    Update clients

--- main.py
+++ main.py
@@ -155,10 +155,10 @@ class Gui(ListTabs):
         self.search_text_variable = []
 
         self.clients_list = [
-            'WEB', 'WEB_EMBED', 'WEB_MUSIC', 'WEB_CREATOR',
-            'ANDROID', 'ANDROID_EMBED', 'ANDROID_MUSIC', 'ANDROID_CREATOR', 'ANDROID_TESTSUITE',
-            'IOS', 'IOS_EMBED', 'IOS_MUSIC', 'IOS_CREATOR',
-            'MWEB', 'TV_EMBED'
+            'WEB', 'WEB_EMBED', 'WEB_MUSIC', 'WEB_CREATOR', 'WEB_SAFARI',
+            'ANDROID', 'ANDROID_MUSIC', 'ANDROID_CREATOR', 'ANDROID_TESTSUITE', 'ANDROID_VR', 'ANDROID_PRODUCER',
+            'IOS', 'IOS_MUSIC', 'IOS_CREATOR',
+            'MWEB', 'TV_EMBED', 'MEDIA_CONNECT'
         ]
         self.clients_selected = 'ANDROID_TESTSUITE'
 
